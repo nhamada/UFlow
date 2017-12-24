eff232e6e3f77fcb518328af52618eb64c012f51
Naohiro Hamada <nao.pinguinus@icloud.com>
2017-12-24 20:30:12 +0900

    Add target dependency
    Add target dep: Yams

--- Package.swift
+++ Package.swift
@@ -15,6 +15,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UFlow",
-            dependencies: []),
+            dependencies: [
+                    .productItem(name: "Yams", package: "Yams"),
+                ]),
     ]
 )
