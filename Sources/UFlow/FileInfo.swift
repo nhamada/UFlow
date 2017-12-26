0410fa29c59bc5df4e85972a96f1017a81a908fa
Naohiro Hamada <nao.pinguinus@icloud.com>
2017-12-27 00:07:26 +0900

    Modify FileInfo
    Fix Inner struct in UFlow -> Top-level struct

--- Sources/UFlow/FileInfo.swift
+++ Sources/UFlow/FileInfo.swift
@@ -7,54 +7,52 @@
 
 import Foundation
 
-public extension UFlow {
+/**
+ * Data container for file info
+ */
+public struct FileInfo {
+    /// Key definition of dictionary containing file info
+    private enum Keys: String {
+        case title = "title"
+        case project = "project"
+        case summary = "summary"
+        case version = "version"
+        case tags = "tags"
+        case date = "date"
+        case authors = "authors"
+    }
+    
+    /// Title of the user flow
+    public let title: String
+    /// Project name which contains this user flow
+    public let project: String?
+    /// Description/summary of the user flow
+    public let summary: String?
+    /// Version string of the user flow
+    public let version: String?
+    /// List of tags assigned to the user flow
+    public let tags: [String]?
+    /// Publication date
+    public let date: Date?
+    /// List of authors
+    public let authors: [String]?
+    
     /**
-     * Data container for file info
+     * Initialize file info from a given dictionary.
+     *
+     * - parameters:
+     *   - dictionary: Dictionary which contains file info
      */
-    public struct FileInfo {
-        /// Key definition of dictionary containing file info
-        private enum Keys: String {
-            case title = "title"
-            case project = "project"
-            case summary = "summary"
-            case version = "version"
-            case tags = "tags"
-            case date = "date"
-            case authors = "authors"
+    public init(from dictionary: [String:Any]) {
+        guard let title = dictionary[Keys.title.rawValue] as? String else {
+            fatalError("Missing `title`")
         }
-        
-        /// Title of the user flow
-        public let title: String
-        /// Project name which contains this user flow
-        public let project: String?
-        /// Description/summary of the user flow
-        public let summary: String?
-        /// Version string of the user flow
-        public let version: String?
-        /// List of tags assigned to the user flow
-        public let tags: [String]?
-        /// Publication date
-        public let date: Date?
-        /// List of authors
-        public let authors: [String]?
-        
-        /**
-         * Initialize file info from a given dictionary.
-         *
-         * - parameters:
-         *   - dictionary: Dictionary which contains file info
-         */
-        public init(from dictionary: [String:Any]) {
-            guard let title = dictionary[Keys.title.rawValue] as? String else {
-                fatalError("Missing `title`")
-            }
-            self.title = title
-            self.project = dictionary[Keys.project.rawValue] as? String
-            self.summary = dictionary[Keys.summary.rawValue] as? String
-            self.version = dictionary[Keys.version.rawValue] as? String
-            self.tags = dictionary[Keys.tags.rawValue] as? [String]
-            self.date = dictionary[Keys.date.rawValue] as? Date
-            self.authors = dictionary[Keys.authors.rawValue] as? [String]
-        }
+        self.title = title
+        self.project = dictionary[Keys.project.rawValue] as? String
+        self.summary = dictionary[Keys.summary.rawValue] as? String
+        self.version = dictionary[Keys.version.rawValue] as? String
+        self.tags = dictionary[Keys.tags.rawValue] as? [String]
+        self.date = dictionary[Keys.date.rawValue] as? Date
+        self.authors = dictionary[Keys.authors.rawValue] as? [String]
     }
 }
