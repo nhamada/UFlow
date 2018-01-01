fd449508221d39a69fbe4e9da43df54b0c434952
Naohiro Hamada <nao.pinguinus@icloud.com>
2018-01-01 17:46:44 +0900

    Add comment
    Add comment to Plotter

--- Sources/uflowc/Plotter.swift
+++ Sources/uflowc/Plotter.swift
@@ -8,8 +8,23 @@
 import Foundation
 import UFlow
 
+/**
+ * User flow plotter protocol.
+ *
+ * To draw or render user flow, a class/struct which conforms this protocol is required.
+ */
 protocol Plotter {
+    /**
+     * File extension of output user flow.
+     */
     static var fileExtension: String { get }
     
+    /**
+     * Draw/Render a given user flow to output directory.
+     *
+     * - parameters:
+     *   - document: User flow to draw/render
+     *   - outputDirecotyr: Output directory
+     */
     static func plot(document: Document, to outputDirectory: URL)
 }
